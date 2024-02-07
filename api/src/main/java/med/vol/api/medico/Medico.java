b41d6029fb8fbd3cb90ba184b1d4f9cf2f9745c5
Mateus Santana <mateus-sgn@hotmail.com>
2024-02-07 08:49:55 -0300

    012 | Adjust Medico.java and MedicoController.java to accept objects

--- api/src/main/java/med/vol/api/medico/Medico.java
+++ api/src/main/java/med/vol/api/medico/Medico.java
@@ -18,7 +18,7 @@ public class Medico {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    private String nome;
     private String email;
     private String crm;
     @Enumerated(EnumType.STRING)
@@ -26,4 +26,11 @@ public class Medico {
     @Embedded
     private Endereco endereco;
 
+    public Medico(DadosCadastroMedico dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
 }
