44a29281c5bfe46cc484a86c7f0459c257f54f4d
Mateus Santana <mateus-sgn@hotmail.com>
2024-02-07 10:52:12 -0300

    021 | Add paciente attributes validation and database creation

--- api/src/main/java/med/vol/api/controller/PacienteController.java
+++ api/src/main/java/med/vol/api/controller/PacienteController.java
@@ -1,9 +1,11 @@
 package med.vol.api.controller;
 
+import jakarta.validation.Valid;
 import med.vol.api.paciente.DadosCadastroPaciente;
 import med.vol.api.paciente.Paciente;
 import med.vol.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +19,8 @@ public class PacienteController {
     private PacienteRepository pacienteRepository;
 
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroPaciente dadosCadastroPaciente) {
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dadosCadastroPaciente) {
         pacienteRepository.save(new Paciente(dadosCadastroPaciente));
     }
 }
