6e9f19ec7a15533365d96541e2196e8d15cc9b58
Mateus Santana <mateus-sgn@hotmail.com>
2024-04-03 18:00:20 -0300

    feat: Add cors configurations, only accept api calls from port 3000

---
+++ api/src/main/java/med/vol/api/configuration/CorsConfiguration.java
@@ -0,0 +1,16 @@
+package med.vol.api.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
