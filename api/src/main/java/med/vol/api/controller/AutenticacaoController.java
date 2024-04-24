52b6532ba17ba1fe4841213e13eb868dac4c9270
Mateus Santana <mateus-sgn@hotmail.com>
2024-04-24 16:57:27 -0300

    feat: Implement authentication endpoint

---
+++ api/src/main/java/med/vol/api/controller/AutenticacaoController.java
@@ -0,0 +1,28 @@
+package med.vol.api.controller;
+
+import jakarta.validation.Valid;
+import med.vol.api.domain.usuario.dto.DadosAutenticacao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dadosAutenticacao) {
+        var token = new UsernamePasswordAuthenticationToken(dadosAutenticacao.login(), dadosAutenticacao.senha());
+        var authentication = authenticationManager.authenticate(token);
+
+        return ResponseEntity.ok().body(authentication);
+    }
+}
